dcab434001cbe03a4b3e9d044e135054ff5beff9
Ayman Abourizq <aabouriz@c3r2p6.1337.ma>
2025-03-14 16:13:02 +0000

    LINUX

--- calculator.cpp
+++ calculator.cpp
@@ -1,4 +1,4 @@
-#incldue <iostream>
+#include <iostream>
 using namespace std;
 
 struct s_tree
@@ -9,18 +9,44 @@ struct s_tree
 	s_tree	*right;
 };
 
+/*
 s_tree	*set_equation_in_tree(char *equation)
 {
-	
+	s_tree	*equation;
+
+
 }
+*/
 
-void	binary_tree_calculator()
+int	binary_tree_calculator(s_tree *equation)
 {
-
+	if (equation->op == '+')
+		return (binary_tree_calculator(equation->left) + binary_tree_calculator(equation->right));
+	else if (equation->op == '-')
+		return (binary_tree_calculator(equation->left) - binary_tree_calculator(equation->right));
+	else if (equation->op == '*')
+		return (binary_tree_calculator(equation->left) * binary_tree_calculator(equation->right));
+	else if (equation->op == '/')
+		return (binary_tree_calculator(equation->left) / binary_tree_calculator(equation->right));
+	else
+		return (equation->value);
 }
 
 int	main(void)
 {
+	s_tree	*equation;
 
+	equation = new s_tree;
+	equation->op = '+';
+	equation->left = new s_tree;
+	equation->left->op = '*';
+	equation->left->left = new s_tree;
+	equation->left->left->value = 5;
+	equation->left->right = new s_tree;
+	equation->left->right->value = 3;
+	equation->right = new s_tree;
+	equation->right->value = 2;
+	// 2 + 5 * 3
+	cout << binary_tree_calculator(equation) << endl;
 	return (0);
 }
