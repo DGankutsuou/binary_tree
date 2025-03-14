47bf0d9ec288fd380d87844bbfc6145517501c1b
Ayman Abourizq <aabouriz@c3r2p6.1337.ma>
2025-03-14 12:59:11 +0000

    LINUX

---
+++ calculator.cpp
@@ -0,0 +1,22 @@
+#incldue <iostream>
+using namespace std;
+
+struct s_tree
+{
+	char	op = 0;
+	int		value = 0;
+	s_tree	*left;
+	s_tree	*right;
+};
+
+
+void	binary_tree_calculator()
+{
+
+}
+
+int	main(void)
+{
+
+	return (0);
+}
