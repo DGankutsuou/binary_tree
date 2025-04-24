41c92c89f5388098a5067e789c9dcb76863fec2d
DGankutsuou <DGankutsuou@github.com>
2025-04-24 15:15:30 +0100

    binary tree done 100%

--- calculator.cpp
+++ calculator.cpp
@@ -5,23 +5,23 @@ using namespace std;
 
 struct s_tree
 {
-	char	op = 0;
-	int		value = 0;
-	s_tree	*left;
-	s_tree	*right;
+	char op = 0;
+	int value = 0;
+	s_tree *left;
+	s_tree *right;
 };
 
 struct s_halfs
 {
-	string left_half;
-	string right_half;
+	string left_half = "";
+	string right_half = "";
 };
 
-vector <string>	spliter(string str, string delim)
+vector<string> spliter(string str, string delim)
 {
-	string	word;
-	short	pos;
-	vector <string> words;
+	string word;
+	short pos;
+	vector<string> words;
 
 	while ((pos = str.find(delim)) != string::npos)
 	{
@@ -35,36 +35,108 @@ vector <string>	spliter(string str, string delim)
 	return (words);
 }
 
-s_halfs	divide_equation(string equation)
+s_halfs divide_equation(string equation, s_tree *&tree, bool &found)
 {
-	s_halfs 		halfs;
-	vector <string>	splited;
-	int				idx = 0;
+	s_halfs halfs;
+	vector<string> splited;
+	int idx = 0;
+	int division = 0;
+	int pcounter = 0;
+	int counter = 0;
+	bool paren = false;
 
 	splited = spliter(equation, " ");
+	// found = false;
+	if (splited[0] == "(")
+	{
+		paren = true;
+		splited.pop_back();
+	}
 	for (string &iter : splited)
 	{
-		if (iter == "+" || iter == "-")
+		if (idx == 0 && paren == true)
 		{
-
+			idx++;
+			continue;
+		}
+		if (iter == "(")
+			pcounter++;
+		else if (iter == ")")
+			pcounter--;
+		if (pcounter == 0 && (iter == "+" || iter == "-"))
+		{
+			found = true;
+			tree->op = iter.at(0);
+			division = idx;
 		}
 		idx++;
 	}
+	if (!found)
+	{
+		idx = 0;
+		division = 0;
+		for (string &iter : splited)
+		{
+			if (idx == 0 && paren == true)
+			{
+				idx++;
+				continue;
+			}
+			if (iter == "(")
+				pcounter++;
+			else if (iter == ")")
+				pcounter--;
+			if (pcounter == 0 && (iter == "*" || iter == "/"))
+			{
+				found = true;
+				tree->op = iter.at(0);
+				division = idx;
+			}
+			idx++;
+		}
+	}
+	if (found)
+	{
+		for (string &iter : splited)
+		{
+			if (counter == 0 && paren == true)
+			{
+				counter++;
+				continue;
+			}
+			if (counter < division)
+			{
+				halfs.left_half += iter + " ";
+			}
+			else if (counter > division)
+				halfs.right_half += iter + " ";
+			counter++;
+		}
+	}
+	else
+	{
+		tree->value = stoi(splited[0]);
+	}
+	return (halfs);
 }
 
-s_tree	*set_equation_in_tree(string equation)
+s_tree *set_equation_in_tree(string equation)
 {
 	s_tree *tree;
 	s_halfs halfs;
+	bool found = false;
 
 	tree = new s_tree;
-	halfs = divide_equation(equation);
-	tree->left = set_equation_in_tree(halfs.left_half);
-	tree->right = set_equation_in_tree(halfs.right_half);
+	halfs = divide_equation(equation, tree, found);
+	if (found == true)
+	{
+		tree->left = set_equation_in_tree(halfs.left_half);
+		tree->right = set_equation_in_tree(halfs.right_half);
+	}
 	return (tree);
 }
 
-int	binary_tree_calculator(s_tree *equation)
+int binary_tree_calculator(s_tree *equation)
 {
 	if (equation->op == '+')
 		return (binary_tree_calculator(equation->left) + binary_tree_calculator(equation->right));
@@ -78,18 +150,18 @@ int	binary_tree_calculator(s_tree *equation)
 		return (equation->value);
 }
 
-int	main(void)
+int main(void)
 {
-	s_tree			*tree;
-	string			equation;
-	vector <string>	splited;
+	s_tree *tree;
+	string equation;
+	vector<string> splited;
 
 	cout << "Enter the equation:\n-> ";
-	cin >> equation;
-	// 2 + 5 * 3
+	getline(cin, equation);
+	//  2 + 5 * 3
 	splited = spliter(equation, " ");
 	tree = set_equation_in_tree(equation);
-	cout << binary_tree_calculator(tree) << endl;
+	cout << "solution: " << binary_tree_calculator(tree) << endl;
 	return (0);
 }
 // to completed someday
